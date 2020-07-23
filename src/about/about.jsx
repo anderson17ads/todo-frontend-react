1496ed777f6395dbe62406f4f5881522a7a77437
Anderson de Souza <anderson17ads@hotmail.com>
2020-07-22 23:05:23 -0300

    Create structure of project

---
+++ src/about/about.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name="About" small="Us" />
+
+        <h2>Our Story</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Labore similique exercitationem, architecto laudantium non aliquid et nemo error? Suscipit laboriosam enim sed quisquam, accusantium explicabo numquam sunt dignissimos alias commodi.</p>
+    </div>
+)
